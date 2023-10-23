ddb06a0d35946d9e4b322f1b9b41ff0d22f1a19f
arbagheri75 <ar.bagheri0075@gmail.com>
2023-10-23 19:29:29 +0330

    add some description for documents

--- simple_progress_bar.py
+++ simple_progress_bar.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 # Print iterations progress
-def print_progress_bar(iteration, total, length=50, prefix='', suffix='', decimal=1, filled='\u2588', blank='-', endbar='\r'):
+def print_progress_bar(iteration, total, length=50, prefix='', suffix='', decimal=1, filled='\u2588', blank='-', print_end='\r'):
     """
     \u2588
     Call in a loop to create a terminal progress bar
@@ -11,8 +11,10 @@ def print_progress_bar(iteration, total, length=50, prefix='', suffix='', decima
         length      - Optional : character length of bar (int)
         prefix      - Optional : prefix string (str)
         suffix      - Optional : suffix string (str)
-        decimal     - Optional : 
-
+        decimal     - Optional : positive number of decimal in percent complete (int)
+        filled      - Optional : bar filled cahracter (str)
+        blank       - Optional : bar blank character (str)
+        print_end   - Optional : end character (e.g. "\r", "\r\n") (str)
     """
 
     percent = ("{0:." + str(decimal) + "f}").format(100 * iteration / total)
@@ -20,7 +22,7 @@ def print_progress_bar(iteration, total, length=50, prefix='', suffix='', decima
     blank_space = length - fill_space
     bar = fill_space * filled + blank_space * blank
 
-    print(f'{prefix} |{bar}|{suffix} {percent}%', end='\r')
+    print(f'{prefix} |{bar}|{percent}% {suffix}', end=print_end)
     if iteration == total:
         print()
 
